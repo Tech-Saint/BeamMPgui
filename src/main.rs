1f4db5fb1eb02e8dce171e51da5594eb358a0e80
Thomas Linkey <tomlewho@gmail.com>
2023-07-18 03:30:09 -0400

    Added open File dialog to app if CFG is empty.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use eframe::egui;
+use rfd::FileDialog;
 use egui::FontTweak;
 use std::io::prelude::*;
 use std::{fs,env,fs::File,path::Path};
 use regex::Regex;
+
+
+
 
 fn main()  -> Result<(), eframe::Error> {
        
@@ -33,7 +37,7 @@ fn main()  -> Result<(), eframe::Error> {
 }
 
 fn read_config() {
-    static Empty_Config: &str ="path_of_Beam=''\npath_of_BeamMP_Executable=''\n";
+    static EMPTYCFG: &str ="path_of_Beam=''\npath_of_BeamMP_Executable=''\n";
     let cfg_path: String;
 
     let current_dir= get_current_working_dir();
@@ -50,6 +54,7 @@ fn read_config() {
     let mut _found: bool= false;
     let mut cfg: String = String::new();
 
+    /* Makes sure that the CFG exists and reads it. */
     while _found != true{
 
         let cfg_temp = fs::read_to_string(&cfg_path);
@@ -67,27 +72,32 @@ fn read_config() {
                     Ok(file) => file,
                 };
 
-                match file.write_all(&Empty_Config.as_bytes()) {
+                match file.write_all(&EMPTYCFG.as_bytes()) {
                     Err(why) => panic!("couldn't write to {}: {}", display, why),
                     Ok(_) => continue
                 }
             }
         };
-    }
+    };
+    /* Read from CFG */
     let re_beamng: Regex = Regex::new(r"Beam='(?<dir>\w+)'").unwrap();
     let re_beammp: Regex = Regex::new(r"BeamMP.*'(?<dir>\w+)'").unwrap();
 
-    let Some(beamng_dir)= re_beamng.captures(&cfg) else {
-        println!("no match!");
-        return;
-    };
-    let Some(beammp_dir)= re_beammp.captures(&cfg) else {
-        println!("no match!");
-        return;
-    };
+    let caps = re_beamng.captures(&cfg);
+        let beamng_dir = match caps {
+            Some(value) => value["dir"].to_string(),
+            None => open_file_explorer("Select your BeamNG folder."),
+        };
+    println!("BeamNG dir: {beamng_dir}");
 
-    println!("{}",&beamng_dir["dir"]);
-    println!("{}",&beammp_dir["dir"]);
+
+    let caps = re_beammp.captures(&cfg);
+    let beammp_dir = match caps {
+        Some(value) => value["dir"].to_string(),
+        None => open_file_explorer("Select your BeamMP folder."),
+    }; 
+
+    println!("BeamMP dir: {beammp_dir}");
 
 }
 
@@ -98,3 +108,23 @@ fn get_current_working_dir() -> String {
         Err(_) => "FAILED".to_string()
     }
 }
+
+fn open_file_explorer( title:&str) -> String{
+    let folder: String;
+    let path = FileDialog::new()
+            .set_title(&title)
+            .set_directory("/")
+            .pick_folder();
+    let _folder=path.unwrap().into_os_string().into_string();
+        folder = match _folder {
+            Ok(string) =>{
+                string
+            },
+            Err(_e) =>{
+                panic!("Fatal Error: Unable to open File explorer!\nPlease manually add the path of your BeamNG and BeamMP Folders to the new Config file.");
+            }
+        };
+    println!("{folder}");
+    return folder;
+}
+
